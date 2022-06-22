4fd90e4ec88777f85e45eba0b35ae4131233959c
Saverio <save.carelli@gmail.com>
2022-06-22 18:43:21 +0200

    Implementazione repository di base

---
+++ src/main/java/it/prova/gestionetratte/repository/airbus/CustomAirbusRepositoryImpl.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetratte.repository.airbus;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public class CustomAirbusRepositoryImpl implements CustomAirbusRepository {
+
+	public List<Airbus> findByExample(Airbus example) {
+		return null;
+	}
+
+}
---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/CustomTrattaRepositoryImpl.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public class CustomTrattaRepositoryImpl implements CustomTrattaRepository {
+
+	public List<Tratta> findByExample(Tratta example) {
+		return null;
+	}
+
+}
