e009e60c2dea69643296d622fd32e89fbd8b6930
Saverio <save.carelli@gmail.com>
2022-06-23 11:03:43 +0200

    Implementazione insert tratta

--- src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
+++ src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
@@ -2,15 +2,20 @@ package it.prova.gestionetratte.web.api;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionetratte.dto.TrattaDTO;
 import it.prova.gestionetratte.model.Tratta;
 import it.prova.gestionetratte.service.TrattaService;
+import it.prova.gestionetratte.web.api.exception.IdNotNullForInsertException;
 import it.prova.gestionetratte.web.api.exception.TrattaNotFoundException;
 
 @RestController
@@ -33,6 +38,17 @@ public class TrattaController {
 			throw new TrattaNotFoundException("Tratta not found con id: " + id);
 
 		return TrattaDTO.buildTrattaDTOFromModel(tratta, true);
+	}
+
+	@PostMapping
+	public TrattaDTO createNew(@Valid @RequestBody TrattaDTO trattaInput) {
+		// se mi viene inviato un id jpa lo interpreta come update ed a me (producer)
+		// non sta bene
+		if (trattaInput.getId() != null)
+			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione");
+
+		Tratta trattaInserita = trattaService.inserisciNuovo(trattaInput.buildTrattaModel());
+		return TrattaDTO.buildTrattaDTOFromModel(trattaInserita, false);
 	}
 
 }
